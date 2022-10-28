7d187b32c4398a71bc32bf847fe948f684f532f1
Kamran <104717242+KamranAbdullaev@users.noreply.github.com>
2022-10-28 16:58:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,237 @@
+const startBtn = document.querySelector('#start'),
+      screens = document.querySelectorAll('.screen');
+
+let huPlayer = "X";
+let aiPlayer = "O";
+let cell = document.querySelectorAll('.cell');
+let aiCo = "red";
+let huCo = "blue";
+
+function statBtn() {
+startBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  screens[0].classList.add('up');
+  buttonSelectionShow();
+  buttonSelection();
+});
+}
+statBtn();
+
+function buttonSelectionShow() {
+  $(".guys, p").css("display", "block");
+  $(".dots").css("display", "flex");
+  $(".dots2").css("display", "flex");
+  $("td").css("display", "show");
+}
+function buttonSelection() {
+  
+$(".dots").click(function () {
+  $(".guys, p").css("display", "none");
+  $("td").css("display", "show");
+  $("table").css("display", "block");
+  aiCo = "blue";
+  huCo = "red";
+  console.log("Red");
+  huPlayer = "X";
+  aiPlayer = "O";
+});
+
+$(".dots2").click(function () {
+  $(".guys, p").css("display", "none");
+  $("td").css("display", "show");
+  $("table").css("display", "block");
+  console.log("blue");
+  huPlayer = "O";
+  aiPlayer = "X";
+  aiCo = "red";
+  huCo = "blue";
+});
+
+}
+
+$("td").click(function () {
+  move(this, huPlayer, huCo);
+  let ai = "ai",
+    human = "human";
+  this.setAttribute('human', "human");
+  if (this.hasAttribute(ai)) {
+  } else if (this.hasAttribute(human)) {
+    this.innerHTML = `${huPlayer}`;
+    this.style.textShadow = "0 0 13px #fff";
+  }
+});
+
+let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let iter = 0;
+let round = 0;
+let = textAboutGame = document.querySelector('.modal__title');
+function move(element, player, color) {
+if (board[element.id] != "X" && board[element.id] != "O") {
+  round++;
+  $(element).css("background-color", color);
+  board[element.id] = player;
+
+if (winning(board, player)) {
+  setTimeout(function () {
+    textAboutGame.innerHTML = `<h1>YOU WIN</h1>`;
+    openModal();
+  }, 500);
+  return;
+} 
+else if (round > 8) {
+  setTimeout(function () {
+    textAboutGame.innerHTML = `<h1>drawn game</h1>`;
+    openModal();
+  }, 500);
+  return;
+} 
+else {
+  round++;
+  let index = minimax(board, aiPlayer).index;
+  let selector = "#" + index;
+  let selectorAi = $(selector);
+  selectorAi.css("background-color", aiCo);
+  selectorAi.css("text-shadow", "0 0 13px #fff");
+  selectorAi.html(aiPlayer);
+  selectorAi.attr('ai', 'ai');
+  board[index] = aiPlayer;
+if (winning(board, aiPlayer)) {
+  setTimeout(function () {
+    textAboutGame.innerHTML = `<h1>YOU LOSE</h1>`;
+    openModal();
+  }, 500);
+  return;
+} else if (round === 0) {
+  setTimeout(function () {
+    textAboutGame.innerHTML = `<h1>drawn game</h1>`;
+    openModal();
+  }, 500);
+    return;
+      }
+    }
+  }
+}
+
+
+function reset() {
+  round = 0;
+  board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  $("td").css("background-color", "transparent");
+  $("td").html("");
+  $(".guys, p").css("display", "show");
+  $("td").css("display", "show");
+  $("table").css("display", "none");
+  $("td").removeAttr("ai");
+  $("td").removeAttr("human");
+  statBtn();
+}
+
+function minimax(reboard, player) {
+  iter++;
+let array = avail(reboard);
+if (winning(reboard, huPlayer)) {
+  return {
+    score: -10
+  };
+} else if (winning(reboard, aiPlayer)) {
+  return {
+    score: 10
+  };
+} else if (array.length === 0) {
+  return {
+    score: 0
+  };
+}
+
+var moves = [];
+for (var i = 0; i < array.length; i++) {
+  var move = {};
+  move.index = reboard[array[i]];
+  reboard[array[i]] = player;
+
+if (player == aiPlayer) {
+  var g = minimax(reboard, huPlayer);
+  move.score = g.score;
+} else {
+  var g = minimax(reboard, aiPlayer);
+  move.score = g.score;
+}
+reboard[array[i]] = move.index;
+moves.push(move);
+}
+
+var bestMove;
+if (player === aiPlayer) {
+  var bestScore = -10000;
+  for (var i = 0; i < moves.length; i++) {
+    if (moves[i].score > bestScore) {
+      bestScore = moves[i].score;
+      bestMove = i;
+    }
+  }
+} else {
+  var bestScore = 10000;
+  for (var i = 0; i < moves.length; i++) {
+    if (moves[i].score < bestScore) {
+      bestScore = moves[i].score;
+      bestMove = i;
+    }
+    }
+  }
+  return moves[bestMove];
+}
+
+function avail(reboard) {
+  return reboard.filter(s => s != "X" && s != "O");
+}
+
+function winning(board, player) {
+  if (
+    (board[0] == player && board[1] == player && board[2] == player) ||
+    (board[3] == player && board[4] == player && board[5] == player) ||
+    (board[6] == player && board[7] == player && board[8] == player) ||
+    (board[0] == player && board[3] == player && board[6] == player) ||
+    (board[1] == player && board[4] == player && board[7] == player) ||
+    (board[2] == player && board[5] == player && board[8] == player) ||
+    (board[0] == player && board[4] == player && board[8] == player) ||
+    (board[2] == player && board[4] == player && board[6] == player)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//**** MODAL */
+let modal = document.querySelector('.modal'),
+    restartBtn = document.querySelector('.btnRestart');
+
+function openModal() {
+  modal.classList.add('show');
+  modal.classList.remove('hide');
+  document.body.style.overflow = 'hidden';
+}
+
+modal.addEventListener('click', (e) => { // Прики клике на заднюю подложку модальное окно закрывается!
+  if (e.target === modal || e.target.getAttribute('data-close') == '') {
+    CloseModal();
+  }
+});
+
+document.addEventListener('keydown', (e) => { // keydown- указывает при нажатий на клавищу 
+  if (e.code === 'Escape' && modal.classList.contains('show')) { // code отслеживает нашу клавищу!
+    CloseModal();
+  }
+});
+restartBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  CloseModal();
+});
+
+function CloseModal() {
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+  document.body.style.overflow = '';
+  screens[0].classList.remove('up');
+  reset();
+}
