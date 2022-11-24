5dc851f6bcea8c62b786a72d9d940aa4d48bd3f5
Kamran <104717242+kamranAbdullaev@users.noreply.github.com>
2022-11-24 16:16:25 +0500

    update js code

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ let cell = document.querySelectorAll('.cell');
 let aiCo = "red";
 let huCo = "blue";
 
-$('.players2').click(function () {
-  screens[0].classList.add('up');
-  screens[1].classList.add('up');
-  twoPlayers();
-});
-
 function statBtn() {
   startBtn.addEventListener('click', (event) => {
     $('.start').css({
@@ -23,7 +17,7 @@ function statBtn() {
       'font-size': '0px',
       'transition': '1.7s'
     });
-    //  $('.mortalLough').html('<audio class="musicAudio"src="https://zvukipro.com/uploads/files/2018-12/1545842847_mji3nde5ntg0mji3nde0_c_2b1cpabqhho.mp3" autoplay></audio>');
+    $('.mortalLough').html('<audio class="musicAudio"src="https://zvukipro.com/uploads/files/2018-12/1545842847_mji3nde5ntg0mji3nde0_c_2b1cpabqhho.mp3" autoplay></audio>');
     event.preventDefault();
     setTimeout(() => {
       screens[0].classList.add('up');
@@ -61,9 +55,9 @@ function buttonSelectionShow() {
   });
 }
 
-/* function MortalComabatFightAudio() {
+function MortalComabatFightAudio() {
   $("td").html('<div><audio class="musicAudio"src="https://zvukipro.com/uploads/files/2018-12/1545842748_nta3mde5ntg0nta3mtew_n4ltgrxccn4.mp3" autoplay></audio></div>');
-} */
+}
 
 function buttonSelection() {
   $(".dots").click(function () {
@@ -74,7 +68,7 @@ function buttonSelection() {
       'transition': '0.5s',
       'font-size': '40px'
     });
-    // MortalComabatFightAudio();
+    MortalComabatFightAudio();
     setTimeout(() => {
       $(".guys, p").css("display", "none");
       $("td").css("display", "show");
@@ -96,7 +90,7 @@ function buttonSelection() {
       'transition': '0.5s',
       'font-size': '40px'
     });
-    //  MortalComabatFightAudio();
+    MortalComabatFightAudio();
     setTimeout(() => {
       $(".guys, p").css("display", "none");
       $("td").css("display", "show");
@@ -196,7 +190,6 @@ function reset() {
   $("table").css("display", "none");
   $("td").removeAttr("ai");
   $("td").removeAttr("human");
-  $('.players2').css({'fontSize':'30px'});
   statBtn();
 }
 
@@ -308,219 +301,4 @@ function CloseModal() {
   document.body.style.overflow = '';
   screens[0].classList.remove('up');
   reset();
-}
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-/* 
-
-
-$('.player1').click(function () {
-  $('.choosePlayer').css('display', 'none');
-  $('.chooseYouPlayer').css('display', 'none');
-  $('.tableForTwoPlayer').css('display', 'grid');
-  oneClickTwoPlayers();
-});
-
-$('.player2').click(function () {
-  $('.choosePlayer').css('display', 'none');
-  $('.chooseYouPlayer').css('display', 'none');
-  $('.tableForTwoPlayer').css('display', 'grid');
-  oneClickTwoPlayers();
-});
-
-
-
-function oneClickTwoPlayers() {
-  $("td").click(addFuncTwoPlayers);
-}
-
-let Player1 = "X";
-let Player2 = "O";
-let p1Color = "red";
-let p2Color = "blue";
-
-function addFuncTwoPlayers() {
-  move2(this, Player1, p1Color);
-  let human2 = "human2",
-    human1 = "human1";
-  this.setAttribute('human1', "human1");
-  if (this.hasAttribute(human2)) { } else if (this.hasAttribute(human1)) {
-    this.innerHTML = `${Player1}`;
-    this.style.textShadow = "0 0 13px #fff";
-  }
-
-}
-let board2 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-let iter2 = 0;
-let round2 = 0;
-
-function move2(element, Player1, color) {
-  if (board2[element.id] != "X" && board2[element.id] != "O") {
-    round2++;
-    $(element).css("background-color", color);
-    board2[element.id] = player;
-
-    if (winning2(board2, player)) {
-      setTimeout(function () {
-        // textAboutGame.innerHTML = `<h1>YOU WIN</h1>`;
-        // openModal();
-        alert('Win');
-      }, 500);
-      return;
-    } else if (round2 > 8) {
-      setTimeout(function () {
-        // textAboutGame.innerHTML = `<h1>drawn game</h1>`;
-        // openModal();
-        alert('Draw');
-      }, 500);
-      return;
-    } else {
-      round2++;
-      function move3(element2, Player2, p2Color) {
-        if (board2[element2.id] != "X" && board2[element.id] != "O") {
-          round2++;
-          $(element).css("background-color", color);
-          board2[element2.id] = Player2;
-
-          if (winning2(board2, Player2)) {
-            setTimeout(function () {
-              // textAboutGame.innerHTML = `<h1>YOU WIN</h1>`;
-              // openModal();
-              alert('Win');
-            }, 500);
-            return;
-          } else if (round2 > 8) {
-            setTimeout(function () {
-              // textAboutGame.innerHTML = `<h1>drawn game</h1>`;
-              // openModal();
-              alert('Draw');
-            }, 500);
-            return;
-          }
-        }
-      }
-    }
-  }
-} */
-function twoPlayers() {
-
-  $('.home').click(() => {
-    screens[0].classList.remove('up');
-    screens[1].classList.remove('up');
-  });
-
-  var buttonStart = document.querySelector(".button-start")
-  var ticTacToe = document.querySelector(".tic-tac-toe");
-  var blockPlayer = document.querySelector(".block-player");
-  var blockNowPlayer = document.querySelector(".block-player div");
-  var gameBlock = document.querySelectorAll(".block");
-  var mass = ["X", "0"];
-  var blockWinner = document.querySelector(".winner");
-
-  buttonStart.onclick = playGame;
-
-  function transformGame() {
-    buttonStart.classList.toggle("hidden-button");
-    ticTacToe.classList.toggle("open-game");
-    blockPlayer.classList.toggle("close-block-player");
-  }
-
-  function endGame() {
-    buttonStart.classList.remove("hidden-button");
-    ticTacToe.classList.remove("open-game");
-    blockPlayer.classList.add("close-block-player");
-    buttonStart.innerHTML = "Играть снова";
-    blockWinner.classList.remove("hidden-winner");
-    setTimeout(function () {
-      for (i = 0; i < gameBlock.length; i++) {
-        gameBlock[i].innerHTML = "";
-      }
-    }, 1000)
-  }
-
-  function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function randomInner() {
-    blockNowPlayer.innerHTML = mass[rand(0, mass.length - 1)]
-  }
-
-  function nextPlayer() {
-    if (blockNowPlayer.innerHTML === mass[0]) {
-      blockNowPlayer.innerHTML = mass[1];
-    }
-    else if (blockNowPlayer.innerHTML === mass[1]) {
-      blockNowPlayer.innerHTML = mass[0];
-    }
-  }
-
-  function indexOfMass(a, m) {
-    var result;
-    for (i = 0; i < m.length; i++) {
-      if (a.indexOf(m[i]) === -1) result = -1;
-    }
-    if (result !== -1) return true;
-    else if (result === -1) return false;
-  }
-
-  function checkWin() {
-    var massWin = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]];
-    var x = [];
-    var y = [];
-    for (i = 0; i < gameBlock.length; i++) {
-      if (gameBlock[i].innerHTML === "X") x.push(i);
-      else if (gameBlock[i].innerHTML === "0") y.push(i);
-    }
-    for (z = 0; z < massWin.length; z++) {
-      if (indexOfMass(x, massWin[z])) {
-        endGame()
-        return ("Победил крестик");
-      }
-
-      if (indexOfMass(y, massWin[z])) {
-        endGame()
-        return ("Победил нолик");
-      }
-    }
-  }
-
-  function stopPlay() {
-    ticTacToe.onclick = function () {
-      return false;
-    }
-  }
-
-  function playGame() {
-    transformGame();
-    blockWinner.classList.add("hidden-winner");
-    randomInner();
-    var count = 0;
-    ticTacToe.onclick = function (event) {
-      if (event.target.className === "block" &&
-        event.target.innerHTML === "") {
-        event.target.innerHTML = blockNowPlayer.innerHTML;
-        nextPlayer();
-        count++;
-        if (checkWin()) {
-          blockWinner.innerHTML = "Игра закончена <br/>" + checkWin();
-        };
-        if (count === 9) {
-          if (!checkWin()) {
-            endGame()
-            blockWinner.innerHTML = "Игра закончилась ничьей";
-          }
-        }
-      }
-    }
-  }
 }
